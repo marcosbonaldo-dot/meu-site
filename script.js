a2dc2eb44d82a7327bb749dbc18fa3f180253388
marcosbonaldo-dot <marcosbonaldo@gmail.com>
2026-05-04 00:24:38 -0300

    ValeDoJaguar

---
+++ script.js
@@ -0,0 +1,5 @@
+function enviarMensagem() {
+  const nome = document.getElementById("nome").value;
+  const mensagem = document.getElementById("mensagem").value;
+  alert("Obrigado, " + nome + "! Sua mensagem foi: " + mensagem);
+}
