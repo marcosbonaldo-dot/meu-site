e076d3d29919e8e3d72a581395aadde7d233f1bb
marcosbonaldo-dot <marcosbonaldo@gmail.com>
2026-05-09 21:36:20 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function enviarMensagem() {
-  const nome = document.getElementById("nome").value;
-  const mensagem = document.getElementById("mensagem").value;
-  alert("Obrigado, " + nome + "! Sua mensagem foi: " + mensagem);
-}
